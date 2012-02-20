857b1e7d21f074cc379fa31253d21a03e4d77522
Jordi Funollet <jordi.f@ati.es>
2012-02-20 17:07:33 +0100

    added tests for read_du_file_maybe()

---
+++ tests/test_base.py
@@ -0,0 +1,17 @@
+from dircloud import read_du_file_maybe
+
+class TestReadDu:
+
+    def test_read_du_file_maybe__key_format(self):
+        # Test file generated with: du /boot
+        du = read_du_file_maybe('tests/fixtures/du.boot')
+        assert du.has_key('boot/') == True
+        assert du.has_key('/boot/') == False
+
+    def test_read_du_file_maybe__key_format_final_slash(self):
+        # Test file generated with: du /boot/
+        du = read_du_file_maybe('tests/fixtures/du.boot_with_final_slash')
+        print du
+        assert 0
+        assert du.has_key('boot/') == True
+        assert du.has_key('/boot/') == False
